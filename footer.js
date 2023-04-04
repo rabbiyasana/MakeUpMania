deb989d3c467a01c19d7a2f11a33bffed17bb4f3
Rabbia sana <rabbiasana55@gmail.com>
2023-04-04 15:14:25 +0500

    add bootstrap to home page and header

--- footer.js
+++ footer.js
@@ -1,42 +1,44 @@
 
 document.getElementById("footer").innerHTML = `
-<footer>
-    <div class="foot">
-        <ul>
-            <li><h5>HELP</h5></li>
-            <li>12345678</li>
-            <li><a href="#">Customer SErvice</a></li>
-            <li><a href="#">Shipping & REturns</a></li>
-            <li>socials</li>
-        </ul>
+<link rel="stylesheet" href="style.css">
+  <div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-12  foot">
+      <ul>
+        <li><h5>HELP</h5></li>
+        <li>12345678</li>
+        <li><a href="#">Customer SErvice</a></li>
+        <li><a href="#">Shipping & REturns</a></li>
+        <li>socials</li>
+      </ul>
     </div>
-    <div class="foot">
-        <ul>
-            <li><h5>ACCOUNT</h5></li>
-            <li>Your Order</li>
-            <li>Status</li>
-            
-        </ul>
+    <div class="col-lg-3 col-md-6 col-sm-12  foot">
+      <ul>
+        <li><h5>ACCOUNT</h5></li>
+        <li>Your Order</li>
+        <li>Status</li>
+      </ul>
     </div>
-    <div class="foot">
-        <ul>
-            <li><h5>ABOUT</h5></li>
-            <li><a href="AboutUs.html">About Us</a></li>
-            <li><a href="Contact.html"></a>Contact</li>
-            <li><a href="team.html"></a>Team</li>
-        </ul>
+    <div class="col-sm-12 col-md-6 col-lg-3 foot">
+      <ul>
+        <li><h5>ABOUT</h5></li>
+        <li><a href="AboutUs.html">About Us</a></li>
+        <li><a href="Contact.html"></a>Contact</li>
+        <li><a href="team.html"></a>Team</li>
+      </ul>
     </div>
-    <div class="foot">
-        <ul>
-            <li><h5>Stay Updated</h5></li>
-            <li><span>info@makeupmania.com</span></li>
-            <li>xyz street road one lahore</li>
-           
-        </ul>
+    <div class="col-lg-3 col-md-6 col-sm-12  foot">
+      <ul>
+        <li><h5>Stay Updated</h5></li>
+        <li><span>info@makeupmania.com</span></li>
+        <li>xyz street road one lahore</li>
+      </ul>
     </div>
-</footer>
-<hr>
-<div id="copyright">
-    <p>Copyright © 2022 Makeup Mania Corporation Limited & Khaadi Pakistan SMC Pvt. Ltd. All Rights Reserved</p>
-     </div>
+    <hr>
+  </div>
+
+<div class="row mt-2">
+<div class="col-lg-12">
+  <p>Copyright © 2022 Makeup Mania Corporation Limited & Khaadi Pakistan SMC Pvt. Ltd. All Rights Reserved</p>
+</div>
+</div>
 `
