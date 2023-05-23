7ac22040654c1229489d523172d2ca378721b809
Rabbia sana <rabbiasana55@gmail.com>
2023-05-24 01:25:07 +0500

    all changes

--- MakeupMania in React/Makeup-mania-with-react/src/App.jsx
+++ MakeupMania in React/Makeup-mania-with-react/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       {/* <ResponsiveAppBar /> */}
-      <NavBar />
+      <p>xsc</p>
       <Home></Home>
     </>
   );
--- MakeupMania in React/Makeup-mania-with-react/src/componenets/navigation/navBar.jsx
+++ MakeupMania in React/Makeup-mania-with-react/src/componenets/navigation/navBar.jsx
@@ -1,8 +1,40 @@
-import * as React from "react";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "bootstrap";
+
 export function NavBar() {
   return (
-    <>
-      <h1>csdcfsdfc</h1>
-    </>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+            <a className="nav-link" href="#">
+              Features
+            </a>
+            <a className="nav-link" href="#">
+              Pricing
+            </a>
+            <a className="nav-link disabled">Disabled</a>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
